ba84fe6c13878d62680b2964a4daa32ce9c0c560
i160267 <87996655+i160267@users.noreply.github.com>
2022-09-19 03:11:55 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,8 @@ streamlit.header('Breakfast Menu New')
 streamlit.text('🍞 Pancakes')
 streamlit.text('🥑 Cheese Omlette, toasts')
 
+streamlit.header('🍞 Build your own Fruit Smoothies 🥑')
+
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
